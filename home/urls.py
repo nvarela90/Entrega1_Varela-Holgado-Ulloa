d8339111ac4a100b1779a1b187fd3dae1dd2e56c
Nicolás Varela <nico.avarela@gmail.com>
2022-10-15 17:45:06 -0300

    Agregados la db, las vistas index, crear y ver jugadores ( se ven pero no tienen funcionalidad) 15/10/22

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    
+    path('', views.index, name= 'index'),
+    path('ver-jugadores/', views.ver_jugadores, name = 'ver-jugadores'),
+    path('crear-jugador/', views.crear_jugador, name = 'crear-jugador')
+    
+ 
+ 
+ 
+    
+]    
